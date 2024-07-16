e86024845d681e1a8fbdcda9643c8e89c589e8a7
AYUSH <ayushkushwaha609@gmail.com>
2024-07-16 23:50:49 +0530

    removed homepage object from packag.json

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Attribution from './components/Attribution';
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
